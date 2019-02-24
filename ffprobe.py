01a0270ed6eb7cfa57d892b5a22beb6fbe86f62a
xiangl18 <43141808+xiangl18@users.noreply.github.com>
2019-02-24 18:27:50 -0500

    Update ffprobe.py

--- ffprobe.py
+++ ffprobe.py
@@ -5,8 +5,11 @@ from pathlib import Path
 
 def ffprobe_sync(filein: Path) -> dict:
 	""" get media metadata """
-	meta_json = subprocess.check_output(['ffprobe', '-v', 'warning', '-print_format',
-'json', '-show_streams', '-show_format', filein], universal_newlines=True)
+	meta_json = subprocess.check_output([
+		'ffprobe', '-v', 'warning', '-print_format', 
+		'json', '-show_streams', '-show_format', filein], 
+		universal_newlines=True
+	)
 	# file = open('test.json{}', 'w', encoding='utf-8')
 	# json.dump(meta_json, file, ensure_ascii=False)
 	return json.loads(meta_json)
