23d488bc9f9b1535b59241ef380d537c1cbeda70
xiangl18 <43141808+xiangl18@users.noreply.github.com>
2019-02-24 18:13:16 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='exercise-2-ffmpeg-xiangl18',
+	version='',
+	description='async ffmpeg exercise',
+	author='Xiang Li',
+	author_email='xiangl18@bu.edu',
+	url='',
+	packages=[''],
+)
