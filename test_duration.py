c4eb336e0bfad52f5920da81afa5bf90eefaf55b
xiangl18 <43141808+xiangl18@users.noreply.github.com>
2019-02-25 00:08:19 -0500

    test video duration

--- test_duration.py
+++ test_duration.py
@@ -5,12 +5,14 @@ from pytest import approx
 def test_duration():
     fnin = 'video.avi'
     fnout = 'outputVideo480p.mp4'
+    fnout2 = 'outputVideo720p.mp4'
 
     orig_meta = ffprobe_sync(fnin)
     orig_duration = float(orig_meta['streams'][0]['duration'])
 
     meta_480 = ffprobe_sync(fnout)
     duration_480 = float(meta_480['streams'][0]['duration'])
-
-    assert orig_duration == approx(duration_480)
+    meta_720 = ffprobe_sync(fnout2)
+    duration_720 = float(meta_720['streams'][0]['duration'])
+    assert orig_duration == approx(duration_480) == approx(duration_720)
 
