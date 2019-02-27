be42a0cfd3be68c72e62ffc958606ff2eb0492ce
xiangl18 <43141808+xiangl18@users.noreply.github.com>
2019-02-27 04:09:15 -0500

    Update main_async.py

--- main_async.py
+++ main_async.py
@@ -10,8 +10,8 @@ FFMPEG = shutil.which('ffmpeg')
 Ntask = 2
 
 # input video and output videos forms
-flist = [{'input': 'video.avi', 'output': 'outputVideo480p.mp4', 'rate': '60', 'fps': '1', 'res': '480'},
-         {'input': 'video.avi', 'output': 'outputVideo720p.mp4', 'rate': '60', 'fps': '1', 'res': '720'}]
+input_list = [{'input': 'video.avi', 'output': 'outputVideo480p.mp4', 'rate': '1', 'fps': '60', 'res': '480'},
+              {'input': 'video.avi', 'output': 'outputVideo720p.mp4', 'rate': '1', 'fps': '60', 'res': '720'}]
 
 # check if there is ffmpeg in the environment
 if not FFMPEG:
@@ -59,32 +59,33 @@ async def ffmpeg(task_queue: asyncio.Queue, task_id: asyncio.Queue):
         except queue.Empty:
             print("no task")
             pass
+        return 
 
 
-async def run():
+async def run(task_list):
     """
     main function to run the whole process.
     """
+    # this code is for multi process based on the cpu logical cores, so far the number of tasks is locked to 2.
     # Ntask = os.cpu_count()  # includes logical cores
     # if not isinstance(Ntask, int):
     #     Ntask = 2
+    
     task_queue = asyncio.Queue()
     tasks = []
     task_id = asyncio.Queue()
-    for f, i in zip(flist, range(Ntask)):
+    for f, i in zip(task_list, range(Ntask)):
         await task_queue.put(f)
         await task_id.put(i)
         tasks.append(asyncio.ensure_future(ffmpeg(task_queue, task_id)))
-
     await task_queue.join()
-
     await asyncio.gather(*tasks, return_exceptions=True)
 
 
 if __name__ == '__main__':
     start = time.time()
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
+    loop.run_until_complete(run(input_list))
     loop.stop()
     end = time.time()
     print('running time:', end)
